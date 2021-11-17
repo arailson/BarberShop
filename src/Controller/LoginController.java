0eab05d61d23fd55c33b6f7f73fc60611571e845
Arailson Santos <arailson.vieira@gmail.com>
2021-11-17 20:47:06 -0300

    Criando Controller e Helpers em Java

---
+++ src/Controller/LoginController.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Controller;
+
+import Controller.Helper.LoginHelper;
+import Model.Usuario;
+import View.Login;
+
+/**
+ *
+ * @author arailson
+ */
+public class LoginController {
+
+    private final Login view;
+    private LoginHelper helper;
+
+    public LoginController(Login view) {
+        this.view = view;
+        this.helper = new LoginHelper(view);
+        
+    }
+    
+    
+    public void entrarNoSistema(){
+        
+        Usuario usuario = helper.obterModelo();
+        
+    }
+    
+    
+    public void fizTarefa(){
+        System.out.println("Busquei algo do banco de dados");
+        
+        this.view.exibeMensagem("Executei o fiz tarefa");
+    }
+    
+}
--- src/View/Login.java
+++ src/View/Login.java
@@ -5,17 +5,27 @@
  */
 package View;
 
+import Controller.LoginController;
+import javax.swing.JOptionPane;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
+
 /**
  *
  * @author tiago
  */
 public class Login extends javax.swing.JFrame {
 
+    private final LoginController controller;
+
     /**
      * Creates new form Login
      */
     public Login() {
         initComponents();
+        controller = new LoginController(this);
+        
+        
     }
 
     /**
@@ -93,7 +103,10 @@ public class Login extends javax.swing.JFrame {
     }//GEN-LAST:event_TextUsuarioActionPerformed
 
     private void ButtonEntrarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_ButtonEntrarActionPerformed
-        // TODO add your handling code here:
+        // Executa quando clico no botão
+        this.controller.fizTarefa();
+       
+        
     }//GEN-LAST:event_ButtonEntrarActionPerformed
 
     private void TextSenhaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TextSenhaActionPerformed
@@ -145,4 +158,30 @@ public class Login extends javax.swing.JFrame {
     private javax.swing.JPasswordField TextSenha;
     private javax.swing.JTextField TextUsuario;
     // End of variables declaration//GEN-END:variables
+
+    public void exibeMensagem(String mensagem) {
+    
+        JOptionPane.showMessageDialog(null, mensagem);
+        
+    }
+
+    public JPasswordField getTextSenha() {
+        return TextSenha;
+    }
+
+    public void setTextSenha(JPasswordField TextSenha) {
+        this.TextSenha = TextSenha;
+    }
+
+    public JTextField getTextUsuario() {
+        return TextUsuario;
+    }
+
+    public void setTextUsuario(JTextField TextUsuario) {
+        this.TextUsuario = TextUsuario;
+    }
+    
+    
+    
+    
 }
